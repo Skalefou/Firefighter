84b50deb0903b3e7a48716875c5ab23ecca84499
Skalefou <skamsagar@gmail.com>
2022-06-28 21:10:44 +0200

    Fixed texture bug

--- src/sprite.cpp
+++ src/sprite.cpp
@@ -7,11 +7,15 @@ description: File managing the functions of the Sprite class which manages sprit
 
 Sprite::Sprite()
 {
-    const std::vector <sf::Vector2f> position {{287.f, 80.f}};
+    const std::vector <sf::Vector2f> position {{287.f, 80.f}, {0.f, 0.f}};
     for(int i = 0; i < std::size(m_nameFileTexture); i++)
     {
         m_sprite.push_back(Texture{});
         m_sprite[i].texture.loadFromFile("data/texture/" + m_nameFileTexture[i]);
+    }
+
+    for(int i = 0; i < std::size(m_sprite); i++)
+    {
         m_sprite[i].sprite.setTexture(m_sprite[i].texture);
         m_sprite[i].sprite.setPosition(position[i]);
     }
