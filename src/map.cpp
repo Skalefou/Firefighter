3463e0cfa83778759f5353152ce9e2a2ae0fd314
Skalefou <skamsagar@gmail.com>
2022-06-29 23:20:57 +0200

    upload

---
+++ src/map.cpp
@@ -0,0 +1,17 @@
+/*
+file: map.cpp
+author: Skalefou
+description: file managing the functions of the Game class that configures the game map as well as its modifications during the game.
+*/
+
+#include "map.hpp"
+
+Map::Map()
+{
+    reset();
+}
+
+void Map::reset()
+{
+    
+}
