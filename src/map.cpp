004a80e671630da9939bdbbce3ac7d101d694567
Skalefou <skamsagar@gmail.com>
2022-07-21 19:32:02 +0200

    Begin programming the maze display

--- src/map.cpp
+++ src/map.cpp
@@ -14,7 +14,8 @@ Map::Map()
 void Map::reset()
 {
     for(int i = 0; i < std::size(m_mapCurrent); i++)
-        m_mapCurrent[i].fill(ALL_DIRECTION_MAP);
+        m_mapCurrent[i].fill(LEFT_MAP);
+    refreshFireDisplay();
 }
 
 int Map::moveKey() const
@@ -53,4 +54,52 @@ void Map::changePositionPlayer(Sprite &sprite, const int move)
     else if (move == UP_PLAYER_MOVE)
         m_positionPlayerMap.y--;
     sprite.changePosition(PLAYER_SPRITE, sf::Vector2f(32*m_positionPlayerMap.x, 32*m_positionPlayerMap.y));
+}
+
+FirePosition Map::setFirePosition(sf::Vector2f pos, bool rotation) const
+{
+    FirePosition a;
+    a.pos = pos;
+    a.rotation = rotation;
+    return a;
+}
+
+void Map::refreshFireDisplay()
+{
+    for (int y = 0; y < 16; y++)
+    {
+        for (int x = 0; x < 16; x++)
+        {
+            int a = m_mapCurrent[x][y];
+            if(a == ALL_DIRECTION_MAP)
+                continue;
+            else if (a == UP_DOWN_MAP)
+            {
+                m_firePosition.push_back(setFirePosition(sf::Vector2f(x*32, y*32), true));
+            }
+            else if (a == LEFT_RIGHT_MAP)
+                m_firePosition.push_back(setFirePosition(sf::Vector2f(x*32, (y*32)-16), false));
+            else if (a == LEFT_MAP)
+            {
+                m_firePosition.push_back(setFirePosition(sf::Vector2f(x*32, (y*32)-16), false));
+                m_firePosition.push_back(setFirePosition(sf::Vector2f(x*32, y*32), true));
+            }
+
+        }
+    }
+}
+
+void Map::display(sf::RenderWindow &window, Sprite &sprite)
+{
+    static bool rotate = false;
+    for(int i = 0; i < std::size(m_firePosition); i++)
+    {
+        if(m_firePosition[i].rotation != rotate && !m_firePosition[i].rotation)
+            sprite.setRotate(FIRE1_SPRITE, 0.f);
+        else if (m_firePosition[i].rotation != rotate && m_firePosition[i].rotation)
+            sprite.setRotate(FIRE1_SPRITE, 90.f);
+        rotate = m_firePosition[i].rotation;
+        sprite.changePosition(FIRE1_SPRITE, m_firePosition[i].pos);
+        sprite.draw(window, FIRE1_SPRITE);
+    }
 }
